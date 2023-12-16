6585bdc64e6fd778593a245be4703865df2ee7f5
Kacher-Abhi <74636212+Kacher-Abhi@users.noreply.github.com>
2023-12-16 12:01:04 +0530

    Add files via upload

---
+++ nursery_management/src/main/java/com/nursery/management/controller/PatientController.java
@@ -0,0 +1,66 @@
+package com.nursery.management.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nursery.management.entity.Patient;
+import com.nursery.management.service.PatientService;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+	@Autowired
+	private PatientService patientService;
+
+	@GetMapping
+	public List<Patient> getAllPatients() {
+		return patientService.getAllPatients();
+	}
+
+	@PostMapping
+	public ResponseEntity<Patient> createPatient(@RequestBody Patient patient) {
+		Patient createdPatient = patientService.createPatient(patient);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdPatient);
+	}
+
+	@GetMapping("/byNursery/{nurseryId}")
+	public ResponseEntity<List<Patient>> getPatientsByNursery(@PathVariable String nurseryId) {
+		List<Patient> patients = patientService.getPatientsByNurseryId(nurseryId);
+		return ResponseEntity.ok(patients);
+	}
+
+	@GetMapping("/{patientId}")
+	public ResponseEntity<Patient> getPatientById(@PathVariable Long patientId) {
+		Patient patient = patientService.getPatientById(patientId);
+
+		if (patient != null) {
+			return ResponseEntity.ok(patient);
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
+
+	@DeleteMapping("/{patientId}")
+	public ResponseEntity<Void> deleteCaretaker(@PathVariable Long patientId) {
+		Patient patient = patientService.getPatientById(patientId);
+
+		if (patient != null) {
+			patientService.deletePatient(patientId);
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
+
+}
