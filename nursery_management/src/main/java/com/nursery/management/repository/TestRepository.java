f3b7ba98cf12a1b34c7043459fa969fe015215df
Kacher-Abhi <74636212+Kacher-Abhi@users.noreply.github.com>
2023-12-16 19:44:06 +0530

    Add files via upload

---
+++ nursery_management/src/main/java/com/nursery/management/repository/TestRepository.java
@@ -0,0 +1,22 @@
+package com.nursery.management.repository;
+
+import java.util.List;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.nursery.management.entity.CareTaker;
+import com.nursery.management.entity.Nursery;
+import com.nursery.management.entity.Patient;
+import com.nursery.management.entity.Test;
+
+public interface TestRepository extends JpaRepository<Test, Long>{
+	
+	
+	List<Test> findByNursery(Nursery nursery);
+	
+	List<Test> findByPatient(Patient patient);
+	
+	List<Test> findByCareTaker(CareTaker careTaker);
+
+
+}
